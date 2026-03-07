73dd99fab4b156d251de27262bccec06ffe7c404
Deepu S <ss.deepu@gmail.com>
2026-03-07 22:43:06 +0000

    When sign in, spinning icon shows in stuck. But i need rotation. Please

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,3 +1,4 @@
+
 // src/app/login/page.tsx
 "use client";
 
@@ -12,7 +13,7 @@ import { SUPER_ADMIN_EMAIL } from "@/lib/config";
 export const dynamic = 'force-dynamic';
 
 const Loader2 = (props: React.SVGProps<SVGSVGElement>) => (
-    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M21 12a9 9 0 1 1-6.219-8.56"/></svg>
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}><path d="M21 12a9 9 0 1 1-6.219-8.56"/></svg>
 );
 
 
