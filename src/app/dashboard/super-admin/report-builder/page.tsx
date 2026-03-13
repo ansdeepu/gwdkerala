df3d3fac08bb94d6962cf7a777fb3b9ed7897276
Deepu S <ss.deepu@gmail.com>
2026-03-13 23:43:01 +0000

    In Super admin profile, Remove page Report Builder

--- src/app/dashboard/super-admin/report-builder/page.tsx
+++
@@ -1,6 +0,0 @@
-// src/app/dashboard/super-admin/report-builder/page.tsx
-import { redirect } from 'next/navigation';
-
-export default function DeprecatedReportBuilderPage() {
-  redirect('/dashboard/report-format-suggestion');
-}
