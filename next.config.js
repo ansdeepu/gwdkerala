1474744a37e2d6795185173a5efa3c9ca2459114
Deepu S <ss.deepu@gmail.com>
2026-04-01 18:46:43 +0000

    Try fixing this error: `Unhandled Runtime Error: Error: Invalid src prop

--- next.config.js
+++ next.config.js
@@ -53,6 +53,12 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'img.youtube.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
   env: {
